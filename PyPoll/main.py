cf77afcf53af4d9d074bd02ae02d9f77d28e006b
SimretGoitom <49961469+SimretGoitom@users.noreply.github.com>
2019-06-03 12:50:18 -0500

    Update main.py
    For some reason my director has issues. Finally, after rebooting VS, it worked just fine.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,47 @@
-# Import Libraries
 import os
 import csv
 
-# Set File Path for Input and Output
-PyPoll = os.path.join("../Resources", "election_data.csv")
-file_output = "Output/poll_results.txt"
-# Declaring the Variables 
-total_votes = 0
-candidates = {}
-candidates_percent = {}
-winner_count = 0
-winner = ""
+#Joining path
+PyPoll =os.path.join('..', 'Resources', 'election_data.csv')
+
+#Define variables
+votes = 0
+candidates = []
+vote_count = {}
+vote_percent = {}
+
+with open(PyPoll, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    
+    
+    #Count votes + list candidates
+    for row in csvreader:
+        
+        votes = votes + 1        
+
+        if row[2] not in candidates and row[2] not in "Candidate":
+            candidates.append(row[2])
+            vote_count[row[2]] = 1
+        elif row[2] in candidates and row[2] not in "Candidate":
+            vote_count[row[2]] = vote_count[row[2]] + 1
+
+    #Calculate percentage of votes
+    for key, value in vote_count.items():
+        vote_percent[key] = str(round(((value/votes)*100),3)) + "% ("+str(value) + ")"
+    
+    #Find winner
+    maxi = max(vote_count.keys(), key=(lambda k: vote_count[k]))    
+    
+    #Make variables for printing results
+    space = "-------------------------------"
+    results1 = (
+        "Election Results" + '\n' + space + '\n' + "Total Votes: "+ str(votes)+ '\n' + space + '\n')
+    results2 = (
+        space + '\n'
+        "Winner: "+ str(maxi)+ '\n' + space + '\n')
+   
+   #Print Results
+    print(results1)
+    for key, val in vote_percent.items():
+        print(key, ": ", val)
+    print(results2)
